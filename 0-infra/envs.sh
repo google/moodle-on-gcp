808920230fd5a0a18e3ba14ccfc6ed62f7783008
Ernani Joppert Pontes Martins <ernanimartins@google.com>
2022-08-22 14:32:58 -0300

    Added some improvements in the setup script and envs.sh

--- 0-infra/envs.sh
+++ 0-infra/envs.sh
@@ -2,7 +2,7 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
@@ -17,7 +17,8 @@
 # make changes to your project, region, zone and networking resources
 # ip ranges here described are suggestive and can be adjusted to fit production's needs
 
-PROJECT=<YOUR-PROJECT-NAME>
+PROJECT_NUMBER=<YOUR-PROJECT-NUMBER>
+PROJECT_ID=<YOUR-PROJECT-ID>
 REGION=<YOUR-PREFERED-REGION>
 ZONE=$REGION-a
 
@@ -26,10 +27,13 @@ SUBNET_NAME=<YOUR-SUBNET-NAME>
 SUBNET_RANGE=10.10.0.0/24
 
 # gke specific variables
-NODE_SA_EMAIL=<YOUR-SERVICE-ACCOUNT-WITH-IAM>@developer.gserviceaccount.com
+NODE_SA_EMAIL=$PROJECT_NUMBER-compute@developer.gserviceaccount.com
 GKE_POD_RANGE=10.168.0.0/14
 GKE_SVC_RANGE=10.172.0.0/19
 GKE_MASTER_IPV4_RANGE=10.10.1.0/28
+
+# cloud build specific variables
+CLOUD_BUILD_SA_EMAIL=$PROJECT_NUMBER@cloudbuild.gserviceaccount.com
 
 # if you have VMs in a different subnet, make sure to include it here, separated by comma (,)
 MASTER_AUTHORIZED_NETWORKS=10.11.0.6/32
@@ -47,7 +51,8 @@ GKE_NAME=<YOUR-GKE-CLUSTER-NAME>
 MYSQL_INSTANCE_NAME=<YOUR-MYSQL-INSTANCE-NAME>
 MYSQL_ROOT_PASSWORD=<YOUR-MYSQL-INSTANCE-PASSWORD>
 MYSQL_DB=<YOUR-MOODLE-DB-NAME>
-COLLATION=utf8mb4_unicode_ci #recommended collation for Moodle. Change only if necessary.
+MYSQL_MOODLE_DB_CHARSET=utf8mb4 #recommended collation for Moodle. Change only if necessary.
+MYSQL_MOODLE_DB_COLLATION=utf8mb4_unicode_ci #recommended collation for Moodle. Change only if necessary.
 
 # other managed services variables
 REDIS_NAME=<YOUR-REDIS-CACHE-NAME>
